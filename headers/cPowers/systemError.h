ea9f49dea44eece553d1970eacc61b89d39e8372
Dylan Powers <dylan.kyle.powers@gmail.com>
2015-09-22 23:52:20 -0700

    Exception-like support added

--- headers/cPowers/systemError.h
+++ headers/cPowers/systemError.h
@@ -1,9 +1,16 @@
 #ifndef SYSTEM_ERRORS_H
 #define SYSTEM_ERRORS_H
 
+#include "errno.h"
+
 typedef enum SystemErr {
-    S_E_CLEAR,
-    S_E_NOMEMS
+  S_E_CLEAR,
+  S_E_NOMEMS
 } SystemErr;
+
+inline void fatal(SystemErr se) {
+  errno = se;
+  raise(SIGABRT);
+}
 
 #endif
