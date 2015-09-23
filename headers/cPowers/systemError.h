295c95d5d628287a55aacd2e0b36f3df48b54e72
Dylan Powers <dylan.kyle.powers@gmail.com>
2015-09-23 00:36:17 -0700

    raiseError is more descriptive than fatal

--- headers/cPowers/systemError.h
+++ headers/cPowers/systemError.h
@@ -2,13 +2,14 @@
 #define SYSTEM_ERRORS_H
 
 #include "errno.h"
+#include "signal.h"
 
 typedef enum SystemErr {
   S_E_CLEAR,
   S_E_NOMEMS
 } SystemErr;
 
-inline void fatal(SystemErr se) {
+inline void raiseError(SystemErr se) {
   errno = se;
   raise(SIGABRT);
 }
