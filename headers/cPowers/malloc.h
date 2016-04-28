91a22ec97820c09365d6e67e2601b3d14ac59258
Dylan Powers <dylan.kyle.powers@gmail.com>
2016-04-27 23:27:24 -0700

    Added my own malloc implementation for mtx

---
+++ headers/cPowers/malloc.h
@@ -0,0 +1,30 @@
+#ifndef MALLOC_H
+#define MALLOC_H
+
+#if __BCC__
+#include "types.h"
+
+#define MALLOC_BUF_SIZE 10000
+
+typedef struct MemRecord {
+  size_t len;
+  struct MemRecord* next;
+} MemRecord;
+
+
+void* malloc(size_t size);
+void* realloc(void* old, size_t size);
+void free(void* ptr);
+
+void initMemRecord();
+void initMemRecordRoot();
+bool MemRecord_isSpaceAvailableAfter();
+MemRecord* MemRecord_allocateAfter();
+MemRecord* MemRecord_findPreviousRecordFor();
+MemRecord* MemRecord_deallocateAfter();
+
+#else
+  #include "stdlib.h"
+#endif
+
+#endif
---
+++ src/malloc.c
@@ -0,0 +1,104 @@
+#include "malloc.h"
+
+#include "systemError.h"
+
+char buf[MALLOC_BUF_SIZE];
+bool initialized = false;
+size_t totalBytesAvailable = MALLOC_BUF_SIZE - sizeof(MemRecord);
+
+
+void* malloc(size_t size) {
+  MemRecord* recordIter;
+  MemRecord* new = NULL;
+  bool spaceAvailableAfter;
+  if (!initialized) {
+    initMemRecordRoot();
+  }
+
+  recordIter = (MemRecord*) buf;
+  spaceAvailableAfter = MemRecord_isSpaceAvailableAfter(recordIter, size);
+  while (recordIter->next != NULL && !spaceAvailableAfter) {
+    recordIter = recordIter->next;
+    spaceAvailableAfter = MemRecord_isSpaceAvailableAfter(recordIter, size);
+  }
+
+  if (spaceAvailableAfter) {
+    new = MemRecord_allocateAfter(recordIter, size);
+  }
+
+  totalBytesAvailable -= new->len + sizeof(MemRecord);
+  printf("Allocated %u bytes @ %x. %u bytes available\n", 
+    new->len, new + 1, totalBytesAvailable);
+  return new + 1;
+}
+
+void* realloc(void* old, size_t size) {
+  void* new;
+  printf("Reallocating\n");
+  new = malloc(size);
+  if (!new) {
+    return NULL;
+  }
+
+  memcpy(new, old, ((MemRecord*) ((char*) old - sizeof(MemRecord)))->len);
+  free(old);
+  return new;
+}
+
+void free(void* ptr) {
+  Err e;
+  MemRecord* m = MemRecord_findPreviousRecordFor((MemRecord*) buf, ptr);
+  if (!m) {
+    e.any = true;
+    sprintf(e.msg, "Invalid ptr to free at %x", ptr);
+    raiseError(&e);
+  }
+
+  totalBytesAvailable += m->next->len + sizeof(MemRecord);
+  printf("Freed %u bytes at %x. %u bytesAvailable\n",
+    m->next->len + sizeof(MemRecord), ptr,
+    totalBytesAvailable);
+  MemRecord_deallocateAfter(m);
+}
+
+void initMemRecord(MemRecord* m, size_t len) {
+  m->len = len;
+  m->next = NULL;
+}
+
+void initMemRecordRoot() {
+  initMemRecord(buf, 0);
+  initialized = true;
+}
+
+
+bool MemRecord_isSpaceAvailableAfter(MemRecord* record, size_t requestSize) {
+  size_t requestedEnd = (char*) (record + 1) + record->len + requestSize + sizeof(MemRecord);
+  return (record->next == NULL && requestedEnd < buf + MALLOC_BUF_SIZE) ||
+    (record->next != NULL && requestedEnd < record->next);
+}
+
+MemRecord* MemRecord_allocateAfter(MemRecord* m, size_t len) {
+  MemRecord* new = (char*) (m + 1) + m->len;
+  MemRecord* next = m->next;
+  initMemRecord(new, len);
+  m->next = new;
+  new->next = next;
+  return new;
+}
+
+MemRecord* MemRecord_findPreviousRecordFor(MemRecord* m, void* ptr) {
+  while (m->next != NULL && m->next + 1 != ptr) {
+    m = m->next;
+  }
+
+  if (m->next == NULL) {
+    return NULL;
+  }
+
+  return m;
+}
+
+MemRecord* MemRecord_deallocateAfter(MemRecord* m) {
+  m->next = m->next->next;
+}
